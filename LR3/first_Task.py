4c0b55f75f37a553554b8a540f10c2b6cf4f91be
Yura <77741895+Yrahtml@users.noreply.github.com>
2026-09-17 16:17:04 +0300

    Update first_Task.py

--- LR3/first_Task.py
+++ LR3/first_Task.py
@@ -4,14 +4,17 @@ def check_three_numbers(a, b, c):
     count = numbers.count(max_num)
     return max_num, count
 
-
 def max_in_range(a, b, step):
     sequence = list(range(a, b + 1, step))
     return max(sequence)
 
+print("Введіть 3 числа які хочете порівняти")
+a=int(input("a = "))
+b=int(input("b = "))
+c =int(input("c = "))
 
-num_max, num_count = check_three_numbers(5, 10, 10)
+num_max, num_count = check_three_numbers(a, b, c)
 print(f"Найбільше число: {num_max}, повторюється разів: {num_count}")
 
-seq_max = max_in_range(2, 15, 3)
+seq_max = max_in_range(a, b, c)
 print(f"Найбільше значення в послідовності: {seq_max}")
